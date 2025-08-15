2deececf420cd8b6e76b1f22356ae38770a76be0
mr258876 <2231327533@qq.com>
2025-08-15 17:13:26 +0800

    switch to a safe vid&pid

--- RTE/USB/USBD_Config_0.c
+++ RTE/USB/USBD_Config_0.c
@@ -34,11 +34,11 @@
 
 //     <o.0..15>Vendor ID <0x0000-0xFFFF>
 //     <i>Vendor ID assigned by USB-IF (idVendor).
-#define USBD0_DEV_DESC_IDVENDOR         0xC251
+#define USBD0_DEV_DESC_IDVENDOR         0x16C0
 
 //     <o.0..15>Product ID <0x0000-0xFFFF>
 //     <i>Product ID assigned by manufacturer (idProduct).
-#define USBD0_DEV_DESC_IDPRODUCT        0x8BCF
+#define USBD0_DEV_DESC_IDPRODUCT        0x05DF
 
 //     <o.0..15>Device Release Number <0x0000-0xFFFF>
 //     <i>Device Release Number in binary-coded decimal (bcdDevice)
